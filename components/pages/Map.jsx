c1a4bd4180012da035d32a6b6a83c815853bf48f
Darren Rogan Personal <darrenrogan@gmail.com>
2022-05-23 16:59:36 +1000

    wip filters

--- components/pages/Map.jsx
+++ components/pages/Map.jsx
@@ -13,6 +13,7 @@ import {
   IonIcon,
   IonContent,
   IonMenuButton,
+  IonFabList
 } from '@ionic/react';
 import { search, filter, bookmark, locate } from 'ionicons/icons';
 import Notifications from './Notifications';
@@ -31,11 +32,23 @@ const Map = () => {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const filterFabRef = useRef(null);
   const [lng, setLng] = useState(139.38479);
   const [lat, setLat] = useState(-20.41969);
   const [distance, setDistance] = useState(1000);
   const [zoom, setZoom] = useState(9);
   const [restAreas, setRestAreas] = useState([]);
+  const [markers, setMarkers] = useState([]);
+
+
+  const [toiletFilter, setToiletFilter] = useState(false);
+  const [waterFilter, setWaterFilter] = useState(false);
+  const [showerFilter, setShowerFilter] = useState(false);
+  const [binFilter, setBinFilter] = useState(false);
+  const [tableFilter, setTableFilter] = useState(false);
+  const [bbqFilter, setBbqFilter] = useState(false);
+  const [fuelFilter, setFuelFilter] = useState(false);
+  const [lightsFilter, setLightsFilter] = useState(false);
 
   // Create a single supabase client for interacting with your database 
   const supabase = createClient('https://arvqjbylexvdpyooykji.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFydnFqYnlsZXh2ZHB5b295a2ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMxMTk1MzUsImV4cCI6MTk2ODY5NTUzNX0.09341SKltY0PCODodzrDD1RQDXB5tA5dnMc-jQbKPag');
@@ -48,9 +61,33 @@ const Map = () => {
   }
 
   const geoSearch = async () => {
-    const { data, error } = await supabase
+    const query = supabase
       .rpc('geo_rest_areas', { x: lng, y: lat, distance: distance })
-      .select()
+      .select();
+
+    if (toiletFilter){
+      query.eq('toilets', true);
+    }
+    if (bbqFilter){
+      query.eq('bbq', true);
+    }
+    if (waterFilter){
+      query.eq('water', true);
+    }
+    if (binFilter){
+      query.eq('bin', true);
+    }
+    if (fuelFilter){
+      query.eq('fuel', true);
+    }
+    if (showerFilter){
+      query.eq('showers', true);
+    }
+    if (lightsFilter){
+      query.eq('lights', true);
+    }
+    
+    const { data, error } = await query;
 
     console.log("supabase lng", lng);
     console.log("supabase lat", lat);
@@ -97,11 +134,18 @@ const Map = () => {
   useEffect(() => {
     if (!map.current) return; // initialize map only once
     console.log("draw markers");
+    markers.map(marker => {
+      marker.remove();
+    });
+    const newMarkers = [];
     restAreas.map(mapRestArea => {
       const marker = new mapboxgl.Marker()
         .setLngLat([mapRestArea.longitude, mapRestArea.latitude])
         .addTo(map.current);
+      newMarkers.push(marker);
     });
+    setMarkers(newMarkers);
+
   }, [restAreas]);
 
   map.current?.on('load', function () {
@@ -138,16 +182,42 @@ const Map = () => {
           </IonFabButton>
         </IonFab>
 
-        <IonFab  vertical="top" horizontal="start" slot="fixed">
-          <IonFabButton  onClick={() => geoMapSearch()}>
-            <IonIcon icon={filter} />
+        <IonFab  vertical="top" horizontal="end" slot="fixed">
+          <IonFabButton color="light" onClick={() => geoMapSearch()}>
+            <IonIcon icon={locate} />
           </IonFabButton>
         </IonFab>
 
-        <IonFab  vertical="top" horizontal="end" slot="fixed">
-          <IonFabButton  onClick={() => geoMapSearch()}>
-            <IonIcon icon={locate} />
+        <IonFab ref={filterFabRef} horizontal="start" vertical="top" slot="fixed" >
+          <IonFabButton color={(waterFilter || toiletFilter || showerFilter || binFilter || tableFilter || fuelFilter || bbqFilter || lightsFilter) ? "dark" : "light" } >
+            <IonIcon icon={filter} />
           </IonFabButton>
+          <IonFabList side="bottom">
+            <IonFabButton color={toiletFilter ? "dark" : "light" } onClick={() => { setToiletFilter(!toiletFilter); }}>
+              <IonIcon src="/svgs/i-toilet.svg" />
+            </IonFabButton>
+            <IonFabButton color={waterFilter ? "dark" : "light" } onClick={() => { setWaterFilter(!waterFilter); }}>
+              <IonIcon src="/svgs/i-water.svg" />
+            </IonFabButton>
+            <IonFabButton color={showerFilter ? "dark" : "light" } onClick={() => { setShowerFilter(!showerFilter); }}>
+              <IonIcon src="/svgs/001-shower.svg" />
+            </IonFabButton>
+            <IonFabButton color={binFilter ? "dark" : "light" } onClick={() => { setBinFilter(!binFilter); }}>
+              <IonIcon src="/svgs/i-rubbish.svg" />
+            </IonFabButton>   
+            <IonFabButton color={tableFilter ? "dark" : "light" } onClick={() => { setTableFilter(!tableFilter); }}>
+              <IonIcon src="/svgs/002-picnic.svg" />
+            </IonFabButton>
+            <IonFabButton color={bbqFilter ? "dark" : "light" } onClick={() => { setBbqFilter(!bbqFilter); }}>
+              <IonIcon src="/svgs/grill.svg" />
+            </IonFabButton>
+            <IonFabButton color={fuelFilter ? "dark" : "light" } onClick={() => { setFuelFilter(!fuelFilter); }}>
+              <IonIcon src="/svgs/fuel.svg" />
+            </IonFabButton>
+            <IonFabButton color={lightsFilter ? "dark" : "light" } onClick={() => { setLightsFilter(!lightsFilter); }}>
+              <IonIcon src="/svgs/i-lighting.svg" />
+            </IonFabButton>
+          </IonFabList>
         </IonFab>
       </IonContent>
     </IonPage>
