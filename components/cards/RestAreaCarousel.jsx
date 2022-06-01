0dc8dabeaeed1ff27996518c81a6c31e92814738
Darren Rogan Personal <darrenrogan@gmail.com>
2022-06-01 17:22:20 +1000

    update

--- components/cards/RestAreaCarousel.jsx
+++ components/cards/RestAreaCarousel.jsx
@@ -1,23 +1,46 @@
-import { Carousel } from "react-responsive-carousel";
+import {
+  IonFab,
+  IonFabButton,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonItem,
+  IonIcon,
+  IonToast,
+  IonLabel,
+  IonContent,
+  IonMenuButton,
+  IonFabList,
+} from '@ionic/react';
 
 export const RestAreaCarousel = (restarea) => {
   if(restarea?.images == undefined || restarea?.images == null || restarea?.images == []) return;
-  const restAreaImages = JSON.parse(restarea?.images);
+  
+  let restAreaImages;
+  try {
+    restAreaImages = JSON.parse(restarea?.images);
+  } catch (error) {
+    console.error("failed to load restarea images", restarea, error);
+  }
+  
 
   console.log("restAreaImages",restAreaImages)
   return (
-    <Carousel showArrows={true} >
+    <div className="w-full" >
+      { restAreaImages ? <IonTitle size="large">Photos</IonTitle> : <IonTitle size="large">No Photos</IonTitle>}
       {
         restAreaImages?.map((image, index) => {
-          console.log('image',  image, index)
           return (
-            <div key={index}>
+            <div key={index} className="w-full my-2 mx-auto">
               <img src={image} alt="rest area image" />
             </div>
           );
         })
       }
-  </Carousel>
+    </div>
   );
 }
     
--- components/cards/RestAreaDetail.jsx
+++ components/cards/RestAreaDetail.jsx
@@ -17,6 +17,7 @@ import {
   IonMenuButton,
   IonFabList,
 } from '@ionic/react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import { search, navigate, bookmark, locate, share, bus, phonePortrait } from 'ionicons/icons';
 import React, { useRef, useEffect, useState } from 'react';
@@ -170,21 +171,31 @@ export const RestAreaDetail = ({restarea}) => {
   
   return (
     <Card className="my-4 mx-auto">
-      <div className="h-32 w-full relative">
-        <img className="h-32 px-auto w-full object-cover object-center" src={img0} alt="image" />
+      <div className="h-64 w-full relative">
+        <img className="h-64 px-auto w-full object-cover object-center" src={img0} alt="image" />
       </div>
       <div className="px-4 py-4 bg-white rounded-b-xl dark:bg-gray-900">
         <h4 className="font-bold py-0 text-s text-gray-400 dark:text-gray-500 uppercase">{restarea.region}</h4>
         <h2 className="font-bold text-2xl text-gray-800 dark:text-gray-100">{restarea.name}</h2>
         <p className="sm:text-sm text-s text-gray-500 mr-1 my-3 dark:text-gray-400">{restarea.road_surface}</p>
 
-        <IonItem color={restarea.toilet ? "dark" : "light" }>
-         <IonIcon slot="end" icon={locate} />
-          <IonLabel className="ion-text-wrap">
-            Longitude: {restarea.longitude} <br/>
-            Latitude: {restarea.latitude}
-          </IonLabel>
-        </IonItem>
+        <CopyToClipboard 
+            text={`${restarea.latitude},${restarea.longitude}`}
+            onCopy={
+              ()=>{
+                setToastMessage("Copied latitude, longitude to clipboard!");
+                setIsToastOpen(true);
+              }
+            }>
+          <IonItem color={restarea.toilet ? "dark" : "light" }>
+            <IonIcon slot="end" icon={locate} />
+            <IonLabel className="ion-text-wrap">
+              Longitude: {restarea.longitude} <br/>
+              Latitude: {restarea.latitude}
+            </IonLabel>
+            <IonLabel color="light" position="floating">Click to Copy to Clipboard</IonLabel>
+          </IonItem>
+        </CopyToClipboard>
 
         <div className="flex items-center space-x-4">
           <h3 className="text-gray-500 dark:text-gray-200 m-l-8 text-md font-medium">{restarea.creator} - {restarea.surface} </h3>
