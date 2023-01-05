268b96b5dc74745a34015a656cdf499fe39ba324
Darren Rogan <darrenrogan@gmail.com>
2023-01-05 13:54:31 +1000

    Revert "Mobile auth - wip"

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -83,7 +83,7 @@ const Menu = () => {
     <IonMenu side="start" contentId="main" onIonDidOpen={handleOpen} onIonDidClose={handleClose} swipeGesture={false}>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>We Watch</IonTitle>
+          <IonTitle>RestSpace</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
