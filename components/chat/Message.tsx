c48c43f612bdbc6ff54765fe87f87c0add5710f3
Darren Rogan Personal <darrenrogan@gmail.com>
2023-01-29 11:19:38 +1000

    added click to trigger flag

--- components/chat/Message.tsx
+++ components/chat/Message.tsx
@@ -12,7 +12,8 @@ import { FabUgcMessageActions } from '../cards/FabUgcMessageActions';
 export const Message = ({ message }) => {
   const user = useUser();
   const [open, setOpen] = useState(false);
-  const [isMyMessage, setIsMyMessage] = useState(false)
+  const [isMyMessage, setIsMyMessage] = useState(false);
+  const [isActionsVisible, setIsActionsVisible] = useState(false);
   const [bgColor, setBgColor] = useState('bg-yellow-500')
   
   useEffect(() => {
@@ -29,21 +30,25 @@ export const Message = ({ message }) => {
   const onDidDismiss = () => {
     console.log('dismiss modal')
   }
+  const toggleFab = () => {
+    console.log('dismiss modal')
+    setIsActionsVisible(!isActionsVisible);
+  }
   
   return (
     <div key={message.id} className={`flex space-x-3 py-4 mt-4 mb-4 p-4 rounded-xl ${bgColor} relative`}>
-        {!isMyMessage && 
-          <FabUgcMessageActions message={message} creator={message?.author}  />
-        }
+       
         <div className="flex-shrink-0" onClick={toggleUserModal}>
             <UserProfileAvatar userProfile={message?.author} /><br/>
             <UserProfile open={open} onDidDismiss={onDidDismiss} userProfile={message?.author} />
         </div>
-        <div className="min-w-0 flex-1">
+        <div className="min-w-0 flex-1" onClick={toggleFab}>
             <p className="text-sm text-gray-800">{message.text}</p>
             <p className="text-sm text-white pt-2">{formatDistanceToNow(new Date(message.inserted_at),{addSuffix: true})}</p>
         </div>    
-        
+        {!isMyMessage && isActionsVisible &&
+          <FabUgcMessageActions message={message} creator={message?.author}  />
+        }
     
     </div>
   )
