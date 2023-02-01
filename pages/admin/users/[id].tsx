73be0809a6484e8867c17983221cd4bc4faaca5b
Darren Rogan Personal <darrenrogan@gmail.com>
2023-02-01 18:11:11 +1000

    shadow ban filtered out for others only

--- pages/admin/users/[id].tsx
+++ pages/admin/users/[id].tsx
@@ -28,8 +28,13 @@ const UserPage = (props) => {
   const shadowBan = (hours: number) => {
     userProfile.banned_to = addHours(new Date(),  hours)
     const res = updateProfile(userProfile, supabase);
+    handleReturn();
+  }
 
-    router.push('/admin/users');
+  const removeAvatar = () => {
+    userProfile.avatar_url = null;
+    const res = updateProfile(userProfile, supabase);
+    handleReturn();
   }
 
 
@@ -124,10 +129,18 @@ const UserPage = (props) => {
     <div className="pt-5">
       <div className="flex justify-end">
         <button
+        onClick={()=> handleReturn()}
           type="button"
           className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-ww-secondary focus:ring-offset-2"
         >
           Cancel
+        </button>
+        <button
+          onClick={()=>removeAvatar()}
+          type="button"
+          className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-ww-secondary focus:ring-offset-2"
+        >
+          Remove Avatar
         </button>
         <button
           onClick={()=>shadowBan(24)}
