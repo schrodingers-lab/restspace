cdd0991b5817408f718d0aa582fe527c672d1c9c
Darren Rogan Personal <darrenrogan@gmail.com>
2023-02-24 12:26:25 +1000

    updated home

--- components/auth/HomeNonUser.tsx
+++ components/auth/HomeNonUser.tsx
@@ -3,6 +3,8 @@ import PhoneInput, { formatPhoneNumber, formatPhoneNumberIntl, isValidPhoneNumbe
 import React, {useState,useRef, useEffect} from 'react';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { Link } from 'react-router-dom';
+import { IonButton } from '@ionic/react';
 
 
 export const HomeNonUser = ({history}) => {
@@ -19,38 +21,40 @@ export const HomeNonUser = ({history}) => {
 
     return (
       <>
-        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gradient-to-br from-fuchsia-500 to-purple-600">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <img
-              className="mx-auto h-12 w-auto"
-              src="/svgs/restspace_logo_blk.svg"
-              alt="RestSpace"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Crime prevention community</h2>
+           <div className="h-full bg-air bg-center bg-cover flex flex-col justify-between py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                className="mx-auto h-12 w-auto"
+                src="/imgs/WeWatch/WeWatch_LogoStrap_white.svg"
+                alt="WeWatch"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100">Community Crime Prevention</h2>
+              
+            </div>
 
-          </div>
+          <div className="mt-4 sm:mx-auto w-full px-4 sm:max-w-md">
+     
+
+
+                <div className="w-full flex flex-col justify-end">
   
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          
   
-                <div>
+              
                   <button
                     type="button"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={goToLogin}
+                    className="flex w-full justify-center mb-8 rounded-md border border-transparent bg-ww-primary py-2 px-4 text-sm font-medium text-white shadow-sm hover:ww-secondary focus:outline-none focus:ring-2 focus:ww-secondary focus:ring-offset-2"
                   >
-                    Login in
+                    Sign In
                   </button>
-                </div> 
 
-                <div>
                   <button
                     type="button"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={goToMaps}
+                    className="flex w-full justify-center my-4 rounded-md bg-white border border-4 border-transparent border-ww-secondary py-2 px-4 text-sm font-medium text-ww-secondary shadow-sm hover:ww-secondary focus:outline-none focus:ring-2 focus:ww-secondary focus:ring-offset-2"
                   >
-                    Maps
+                    Live Map
                   </button>
-                </div>
             </div>
           </div>
         </div>
