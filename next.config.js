69f96c9c2708b7123a30948c69e1db20fd9177b0
Darren Rogan Personal <darrenrogan@gmail.com>
2023-04-13 21:26:30 +1000

    fix fs error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   experimental: {
     appDir: false,
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
   }
 };
