5e27ee86375db9ce5557e0a2edc60fb5878b811d
Darren Rogan Personal <darrenrogan@gmail.com>
2023-04-29 16:16:48 +1000

    added reminder to change keys after moving account

--- pages/api/notifier/notification/[id].tsx
+++ pages/api/notifier/notification/[id].tsx
@@ -5,6 +5,7 @@ import admin from 'firebase-admin';
 // Initialize Supabase client
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
+//TODO move to vercel env vars (before going live)
 // Initialize Firebase Admin SDK
 const serviceAccount = require('./wewatchapp-7d13a-firebase-adminsdk-6do2h-8507094c5d.json');
 admin.initializeApp({
