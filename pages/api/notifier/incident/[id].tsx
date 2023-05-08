fc9c34e3130cfc9d748b488b48057593e52b242a
Darren Rogan Personal <darrenrogan@gmail.com>
2023-05-08 17:57:29 +1000

    test 1

--- pages/api/notifier/incident/[id].tsx
+++ pages/api/notifier/incident/[id].tsx
@@ -6,18 +6,19 @@ import admin from 'firebase-admin';
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 // Initialize Firebase Admin SDK
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
-  "private_key":  process.env.FIREBASE_PROJECT_KEY,
-  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X_CERT_URL,
-} as any;
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT as any;
+// const serviceAccount = {
+//   "type": "service_account",
+//   "project_id": process.env.FIREBASE_PROJECT_ID,
+//   "private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
+//   "private_key":  process.env.FIREBASE_PROJECT_KEY,
+//   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//   "client_id": process.env.FIREBASE_CLIENT_ID,
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": process.env.FIREBASE_CLIENT_X_CERT_URL,
+// } as any;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
