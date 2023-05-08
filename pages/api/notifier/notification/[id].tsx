2d7fc4a24da86bac99abe63a636de86ea3e12223
Darren Rogan Personal <darrenrogan@gmail.com>
2023-05-08 17:23:05 +1000

    update the service account to the env parameters

--- pages/api/notifier/notification/[id].tsx
+++ pages/api/notifier/notification/[id].tsx
@@ -7,7 +7,21 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.
 
 //TODO move to vercel env vars (before going live)
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./wewatchapp-7d13a-firebase-adminsdk-6do2h-8507094c5d.json');
+const serviceAccount1 = require('./wewatchapp-7d13a-firebase-adminsdk-6do2h-8507094c5d.json');
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
+  "private_key":  process.env.FIREBASE_PROJECT_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X_CERT_URL,
+} as any;
+
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
