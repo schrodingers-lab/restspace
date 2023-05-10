5de8d6640d7a37acc532e1791cb006f73832bc1c
Darren Rogan Personal <darrenrogan@gmail.com>
2023-05-10 15:50:08 +1000

    removed comments

--- pages/api/notifier/notification/[id].tsx
+++ pages/api/notifier/notification/[id].tsx
@@ -5,20 +5,8 @@ import admin from 'firebase-admin';
 // Initialize Supabase client
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-
 // Initialize Firebase Admin SDK
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
-  "private_key":  process.env.FIREBASE_PROJECT_KEY,
-  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X_CERT_URL,
-} as any;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 
 admin.initializeApp({
